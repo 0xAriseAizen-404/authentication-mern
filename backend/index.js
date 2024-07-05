bae16a461cfabb10290cacc1565f8abca5027501
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-07-06 01:52:19 +0530

    pre deployment changes

--- backend/index.js
+++ backend/index.js
@@ -5,23 +5,33 @@ import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
+
+const __dirname = path.resolve();
+
+dotenv.config();
+connectDB();
 
 const app = express();
-dotenv.config();
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+);
+
 const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
   console.log(`App is listening on port - ${PORT}`);
-  connectDB();
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "Api is working" });
+  res.json({ message: "API is working" });
 });
-
-app.use("/api/user", userRoutes);
-app.use("/api/auth", authRoutes);
